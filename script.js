e58e3fa9a3c7829dd3099b1bdb6e5808047db76c
Alteredskull <johnson.diantai@gmail.com>
2020-01-25 23:48:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+console.log("Hey");
+
+// display a list of questions
+
+// assign them values
+
+// one is true while the others are false
+
+// display correct and false
+
+// store id/initials for user
+
+// ad time to start when the game starts
+
+// add time for correct answers
+
+// remove time for incorrect answers 
+
+// display final result
+
+var counter = 60;
+
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "parentheses"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for leaps", "console.log"],
+        answer: "console.log"
+    },
+  ];
+
+  console.log(questions[3].title);
+
+function initiate(){
+
+}
+
+function restart(){
+    
+}
